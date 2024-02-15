1d6fa131e3c73cbf9f72d4f2ecdda564977d5e72
Donovan Glover <donovan@dglover.co>
2024-02-15 13:08:32 -0500

    meta: Simplify prepare script

--- src/build/main.ts
+++ src/build/main.ts
@@ -1,7 +1,11 @@
 import { getSchemesFromPath } from './lib'
+import { mkdir, writeFile } from 'fs/promises'
+
+const outputDirectory = './src/dist'
 
 void (async () => {
   const schemes = await getSchemesFromPath('./schemes/base16')
 
-  console.log(JSON.stringify(schemes))
+  await mkdir(outputDirectory, { recursive: true })
+  await writeFile(`${outputDirectory}/schemes.json`, JSON.stringify(schemes) + '\n', 'utf-8')
 })()
