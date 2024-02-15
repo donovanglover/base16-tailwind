373cfcc6afea855a2d005c51c254ef24324b9e97
Donovan Glover <donovan@dglover.co>
2024-02-15 12:49:58 -0500

    jest: Show test coverage when running tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from 'jest'
 
 const config: Config = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/lib.ts'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     '~build': '<rootDir>/src/build/lib.ts',
