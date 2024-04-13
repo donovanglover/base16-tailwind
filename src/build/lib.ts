5f72301b2ef772e4ac3a322a4158de9bf56d625d
Donovan Glover <donovan@dglover.co>
2024-04-13 16:23:11 -0400

    build: Inline types

--- src/build/lib.ts
+++ src/build/lib.ts
@@ -1,7 +1,29 @@
 import { readdir, readFile, lstat } from 'fs/promises'
 import { join } from 'path'
 import YAML from 'yaml'
-import { type Color, type Palette, type HEX } from './types'
+
+export enum Color {
+  base00 = 'base00',
+  base01 = 'base01',
+  base02 = 'base02',
+  base03 = 'base03',
+  base04 = 'base04',
+  base05 = 'base05',
+  base06 = 'base06',
+  base07 = 'base07',
+  base08 = 'base08',
+  base09 = 'base09',
+  base0A = 'base0A',
+  base0B = 'base0B',
+  base0C = 'base0C',
+  base0D = 'base0D',
+  base0E = 'base0E',
+  base0F = 'base0F'
+}
+
+export type HEX = `#${string}`
+
+export type Palette = Record<Color, HEX>
 
 const isFile = async (fileName: string): Promise<boolean> => {
   return (await lstat(fileName)).isFile()
