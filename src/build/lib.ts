2c93fc7e32c393fdd12a9ea4d311dbc2f38f2407
Donovan Glover <donovan@dglover.co>
2024-04-13 16:41:17 -0400

    meta: Merge build with lib and simplify types
    End goal is only having to run one script to create the CSS.

--- src/build/lib.ts
+++ src/build/lib.ts
@@ -1,29 +1,9 @@
 import fs from 'fs/promises'
 import { join } from 'path'
 import YAML from 'yaml'
+import { isLight } from 'color-2-name'
 
-export enum Color {
-  base00 = 'base00',
-  base01 = 'base01',
-  base02 = 'base02',
-  base03 = 'base03',
-  base04 = 'base04',
-  base05 = 'base05',
-  base06 = 'base06',
-  base07 = 'base07',
-  base08 = 'base08',
-  base09 = 'base09',
-  base0A = 'base0A',
-  base0B = 'base0B',
-  base0C = 'base0C',
-  base0D = 'base0D',
-  base0E = 'base0E',
-  base0F = 'base0F'
-}
-
-export type HEX = `#${string}`
-
-export type Palette = Record<Color, HEX>
+type Palette = Record<string, string>
 
 const isFile = async (fileName: string): Promise<boolean> => {
   return (await fs.lstat(fileName)).isFile()
@@ -41,7 +21,7 @@ export async function getSchemesFromPath (folderPath: string): Promise<Record<st
       const schemeName = fileName.split('.yaml')[0]
       const { palette }: { palette: Palette } = YAML.parse(fileContents)
 
-      for (const [key, value] of Object.entries(palette) as Array<[Color, HEX]>) {
+      for (const [key, value] of Object.entries(palette)) {
         palette[key] = `#${value}`
       }
 
@@ -56,6 +36,48 @@ const outputDirectory = './src/dist'
 
 void (async () => {
   const schemes = await getSchemesFromPath('./schemes/base16')
+
+  const colorsArray: Record<string, Record<string, string>> = {}
+
+  for (const [colorName, colorValues] of Object.entries(schemes)) {
+    const colorShades = isLight(colorValues.base00)
+      ? {
+          800: colorValues.base07,
+          700: colorValues.base06,
+          600: colorValues.base05,
+          500: colorValues.base04,
+          400: colorValues.base03,
+          300: colorValues.base02,
+          200: colorValues.base01,
+          100: colorValues.base00
+        }
+      : {
+          800: colorValues.base00,
+          700: colorValues.base01,
+          600: colorValues.base02,
+          500: colorValues.base03,
+          400: colorValues.base04,
+          300: colorValues.base05,
+          200: colorValues.base06,
+          100: colorValues.base07
+        }
+
+    colorsArray[colorName] = {
+      transparent: 'transparent',
+      current: 'currentColor',
+      ...colorShades,
+      red: colorValues.base08,
+      orange: colorValues.base09,
+      yellow: colorValues.base0A,
+      green: colorValues.base0B,
+      cyan: colorValues.base0C,
+      blue: colorValues.base0D,
+      purple: colorValues.base0E,
+      pink: colorValues.base0F
+    }
+  }
+
+  console.log(colorsArray)
 
   await fs.mkdir(outputDirectory, { recursive: true })
   await fs.writeFile(`${outputDirectory}/schemes.json`, JSON.stringify(schemes) + '\n', 'utf-8')
