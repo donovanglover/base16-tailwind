8cfad8ab01cdbcec6dd774302de45f128b449363
Donovan Glover <donovan@dglover.co>
2024-04-14 22:47:14 -0400

    chore: Simplify formatting
    Removed unnecessary comments and parentheses and simplified object
    order.

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,19 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  verbose: true,
   collectCoverage: true,
+
   collectCoverageFrom: [
     './src/**/*.ts'
   ],
+
   coveragePathIgnorePatterns: [
     './src/types/Base16ColorName.ts'
   ],
+
   setupFilesAfterEnv: [
     './tests/_customMatchers.ts'
   ]
--- src/lib/hexToRgb.ts
+++ src/lib/hexToRgb.ts
@@ -1,8 +1,6 @@
 import { type Base16ColorValue } from '../types/Base16ColorValue'
 import { colord } from 'colord'
 
-// Given: color in "#AABBCC"
-// Returns: "38 94 122"
 export function hexToRgb (color: Base16ColorValue): string {
   return colord(color).toRgbString().split('(')[1].split(')')[0].replaceAll(',', '')
 }
