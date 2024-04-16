b7b294c95fc00dc2f664ec025818279a34db2186
Donovan Glover <donovan@dglover.co>
2024-04-16 17:37:54 -0400

    feat: Replace Base16Yaml interface with class
    Makes it easier to directly throw errors if something is wrong instead
    of warning and continuing. Should also make things easier to test since
    we don't have to continuously check whether we failed previously or not.

--- src/Base16Yaml.ts
+++ src/Base16Yaml.ts
@@ -1,24 +1,42 @@
 import { type Base16Palette, isBase16Palette } from './Base16Palette.ts'
 
-export interface Base16Yaml {
-  system: 'base16'
-  name: string
-  slug?: string
-  author: string
-  variant: 'light' | 'dark'
-  palette: Base16Palette
-}
+const BASE16_YAML_HELP = 'Are you sure your .yaml file follows standards?'
 
-export function isBase16Yaml (maybeBase16Yaml: unknown): maybeBase16Yaml is Base16Yaml {
-  const yaml = maybeBase16Yaml
+export class Base16Yaml {
+  readonly system = 'base16'
+  readonly name: string
+  readonly slug?: string
+  readonly author: string
+  readonly variant: 'light' | 'dark'
+  readonly palette: Base16Palette
 
-  return (
-    yaml !== null && typeof yaml === 'object' &&
-    'system' in yaml && yaml.system === 'base16' &&
-    'name' in yaml && typeof yaml.name === 'string' &&
-    ('slug' in yaml ? typeof yaml.slug === 'string' : true) &&
-    'author' in yaml && typeof yaml.author === 'string' &&
-    'variant' in yaml && (yaml.variant === 'light' || yaml.variant === 'dark') &&
-    'palette' in yaml && isBase16Palette(yaml.palette)
-  )
+  constructor (maybeBase16Yaml: unknown) {
+    if (maybeBase16Yaml === null || typeof maybeBase16Yaml !== 'object') {
+      throw new Error(`A non-object value was given as a Base16 yaml. ${BASE16_YAML_HELP}`)
+    }
+
+    if (!Base16Yaml.isValid(maybeBase16Yaml)) {
+      throw new Error(`Invalid Base16 yaml "${JSON.stringify(maybeBase16Yaml)}" was given. ${BASE16_YAML_HELP}`)
+    }
+
+    this.name = maybeBase16Yaml.name
+    this.slug = maybeBase16Yaml.slug
+    this.author = maybeBase16Yaml.author
+    this.variant = maybeBase16Yaml.variant
+    this.palette = maybeBase16Yaml.palette
+  }
+
+  static isValid (maybeBase16Yaml: unknown): maybeBase16Yaml is Base16Yaml {
+    const yaml = maybeBase16Yaml
+
+    return (
+      yaml !== null && typeof yaml === 'object' &&
+      'system' in yaml && yaml.system === 'base16' &&
+      'name' in yaml && typeof yaml.name === 'string' &&
+      ('slug' in yaml ? typeof yaml.slug === 'string' : true) &&
+      'author' in yaml && typeof yaml.author === 'string' &&
+      'variant' in yaml && (yaml.variant === 'light' || yaml.variant === 'dark') &&
+      'palette' in yaml && isBase16Palette(yaml.palette)
+    )
+  }
 }
--- src/getSchemesFromPath.ts
+++ src/getSchemesFromPath.ts
@@ -2,7 +2,7 @@ import { lstatSync, readdirSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import YAML from 'yaml'
 import { Base16Scheme } from './Base16Scheme.ts'
-import { isBase16Yaml } from './Base16Yaml.ts'
+import { Base16Yaml } from './Base16Yaml.ts'
 
 export function getSchemesFromPath (folderPath: string): Base16Scheme[] {
   const schemes = []
@@ -13,20 +13,15 @@ export function getSchemesFromPath (folderPath: string): Base16Scheme[] {
 
     if (lstatSync(filePath).isFile() && filePath.endsWith('.yaml')) {
       const fileContents = readFileSync(filePath, 'utf-8')
-      const yaml = YAML.parse(fileContents)
+      const yaml = new Base16Yaml(YAML.parse(fileContents))
+      const scheme = new Base16Scheme(yaml)
+      const fileNameSlug = fileName.replace('.yaml', '')
 
-      if (isBase16Yaml(yaml)) {
-        const scheme = new Base16Scheme(yaml)
-        const fileNameSlug = fileName.replace('.yaml', '')
-
-        if (!scheme.name.includes(fileNameSlug)) {
-          console.warn(`WARNING(base16-tailwind): Got a className of ${scheme.name} for ${fileName}. This means that the slug from the YAML data differs from the slug of the file name.`)
-        }
-
-        schemes.push(scheme)
-      } else {
-        console.warn(`WARNING(base16-tailwind): Got invalid Base16Yaml from file ${fileName}. It will not be included in the Tailwind CSS output.`)
+      if (!scheme.name.includes(fileNameSlug)) {
+        console.warn(`WARNING(base16-tailwind): Got a className of ${scheme.name} for ${fileName}. This means that the slug from the YAML data differs from the slug of the file name.`)
       }
+
+      schemes.push(scheme)
     }
   }
 
