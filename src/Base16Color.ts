de7e69a7ac5709a863913bff0a0100dfc23b6513
Donovan Glover <donovan@dglover.co>
2024-04-16 16:34:25 -0400

    fix: Make Base16Color support any type with unknown
    Fixes an issue where one of the values being passed into Base16Color
    couldn't be determined at compile time.

--- src/Base16Color.ts
+++ src/Base16Color.ts
@@ -1,11 +1,17 @@
 import { colord } from 'colord'
 
+const BASE16_COLOR_HELP = 'Make sure that your color is in the format "FA113D" where there are 6 hexadecimal numbers without "#" at the beginning.'
+
 export class Base16Color {
   readonly #color: string
 
-  constructor (maybeBase16Color: string) {
+  constructor (maybeBase16Color: unknown) {
+    if (typeof maybeBase16Color !== 'string') {
+      throw new Error(`A non-string value was given as a Base16 color. ${BASE16_COLOR_HELP}`)
+    }
+
     if (!Base16Color.isValid(maybeBase16Color)) {
-      throw new Error(`Invalid Base16 color "${maybeBase16Color}" was given. Make sure that your color is in the format "FA113D" where there are 6 hexadecimal numbers without "#" at the beginning.`)
+      throw new Error(`Invalid Base16 color "${maybeBase16Color}" was given. ${BASE16_COLOR_HELP}`)
     }
 
     this.#color = maybeBase16Color
