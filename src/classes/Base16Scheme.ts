af0f5553eee9c3fb649c63836d52eab4a73608e3
Donovan Glover <donovan@dglover.co>
2024-04-16 09:16:59 -0400

    refactor: Replace not || with &&

--- src/classes/Base16Scheme.ts
+++ src/classes/Base16Scheme.ts
@@ -36,15 +36,15 @@ const BASE16_PALETTE_KEYS = JSON.stringify(
 function isBase16Yaml (maybeBase16Yaml: unknown): maybeBase16Yaml is Base16Yaml {
   const yaml = maybeBase16Yaml
 
-  return (!(
-    yaml === null || typeof yaml !== 'object' ||
-    !('system' in yaml) || yaml.system !== 'base16' ||
-    !('name' in yaml) || typeof yaml.name !== 'string' ||
-    ('slug' in yaml && typeof yaml.slug !== 'string') ||
-    !('author' in yaml) || typeof yaml.author !== 'string' ||
-    !('variant' in yaml) || (yaml.variant !== 'light' && yaml.variant !== 'dark') ||
-    !('palette' in yaml) || !isBase16Palette(yaml.palette)
-  ))
+  return (
+    yaml !== null && typeof yaml === 'object' &&
+    'system' in yaml && yaml.system === 'base16' &&
+    'name' in yaml && typeof yaml.name === 'string' &&
+    ('slug' in yaml ? typeof yaml.slug === 'string' : true) &&
+    'author' in yaml && typeof yaml.author === 'string' &&
+    'variant' in yaml && (yaml.variant === 'light' || yaml.variant === 'dark') &&
+    'palette' in yaml && isBase16Palette(yaml.palette)
+  )
 }
 
 function isBase16Palette (maybeBase16Palette: unknown): maybeBase16Palette is Base16Palette {
