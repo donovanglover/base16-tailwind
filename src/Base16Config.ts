60dcf4ff08ddc384277713737784d6e48b34c0d1
Donovan Glover <donovan@dglover.co>
2024-04-16 18:47:48 -0400

    feat: Convert base16Config object to class
    This should work and enable us to support options that the user can
    customize the config with.

--- src/Base16Config.ts
+++ src/Base16Config.ts
@@ -1,20 +1,33 @@
 import typographyPlugin from '@tailwindcss/typography'
 import type { Config } from 'tailwindcss'
-import type { PluginAPI } from 'tailwindcss/types/config'
+import type { ContentConfig, CustomThemeConfig, PluginAPI, PluginsConfig } from 'tailwindcss/types/config'
 import { base16Plugin } from './base16Plugin.ts'
 
-export const base16Config: Config = {
-  content: [
+export interface Base16ConfigOptions {
+  withTypography: boolean
+  withFontOverride: boolean
+}
+
+export class Base16Config implements Config {
+  content: ContentConfig = [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+  ]
 
-  theme: {
+  theme: Partial<CustomThemeConfig> = {
     colors: {
       transparent: 'transparent',
-      current: 'currentColor',
+      current: 'currentColor'
+    }
+  }
 
+  plugins: PluginsConfig = []
+
+  constructor (options: Base16ConfigOptions) {
+    this.plugins.push(base16Plugin)
+
+    this.theme.colors = {
       100: 'rgb(var(--color-100) / <alpha-value>)',
       200: 'rgb(var(--color-200) / <alpha-value>)',
       300: 'rgb(var(--color-300) / <alpha-value>)',
@@ -23,7 +36,6 @@ export const base16Config: Config = {
       600: 'rgb(var(--color-600) / <alpha-value>)',
       700: 'rgb(var(--color-700) / <alpha-value>)',
       800: 'rgb(var(--color-800) / <alpha-value>)',
-
       red: 'rgb(var(--color-red) / <alpha-value>)',
       orange: 'rgb(var(--color-orange) / <alpha-value>)',
       yellow: 'rgb(var(--color-yellow) / <alpha-value>)',
@@ -32,10 +44,20 @@ export const base16Config: Config = {
       blue: 'rgb(var(--color-blue) / <alpha-value>)',
       purple: 'rgb(var(--color-purple) / <alpha-value>)',
       pink: 'rgb(var(--color-pink) / <alpha-value>)'
-    },
+    }
 
-    extend: {
-      typography: ({ theme }: PluginAPI) => ({
+    if (options.withFontOverride) {
+      this.theme.extend.fontFamily = {
+        sans: ['var(--font-sans)'],
+        serif: ['var(--font-serif)'],
+        mono: ['var(--font-mono)']
+      }
+    }
+
+    if (options.withTypography) {
+      this.plugins.push(typographyPlugin)
+
+      this.theme.extend.typography = ({ theme }: PluginAPI) => ({
         DEFAULT: {
           css: {
             '--tw-prose-body': theme('colors[100]'),
@@ -56,18 +78,7 @@ export const base16Config: Config = {
             '--tw-prose-td-borders': theme('colors[100]')
           }
         }
-      }),
-
-      fontFamily: {
-        sans: ['var(--font-sans)'],
-        serif: ['var(--font-serif)'],
-        mono: ['var(--font-mono)']
-      }
+      })
     }
-  },
-
-  plugins: [
-    typographyPlugin,
-    base16Plugin
-  ]
+  }
 }
