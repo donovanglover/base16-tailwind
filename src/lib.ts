0fe9c2aadeb24022e65b71b0c4035ff6affe6abf
Donovan Glover <donovan@dglover.co>
2024-04-17 05:09:40 -0400

    chore: Abstract option logic into separate file
    Should help keep the actual plugin part separate from the option part.

--- src/lib.ts
+++ src/lib.ts
@@ -1,2 +1,16 @@
-export { Base16Config } from './Base16Config.ts'
-export { base16Plugin } from './base16Plugin.ts'
+import plugin from 'tailwindcss/plugin'
+import type { Config, PluginCreator } from 'tailwindcss/types/config'
+import { Base16Config, type Base16ConfigOptions } from './Base16Config.ts'
+import { Base16Plugin } from './base16Plugin.ts'
+
+function base16Handler (options?: Base16ConfigOptions): PluginCreator {
+  return new Base16Plugin(options).creator
+}
+
+function base16Config (options?: Base16ConfigOptions): Config {
+  return new Base16Config(options)
+}
+
+export const base16Tailwind = plugin.withOptions(base16Handler, base16Config)
+
+export default base16Tailwind
