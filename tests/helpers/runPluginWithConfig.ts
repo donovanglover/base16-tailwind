3916b5f4d81f7ca949b6cac225e6b8e785b31fbc
Donovan Glover <donovan@dglover.co>
2024-04-17 05:58:56 -0400

    tests: Make passing Base16Options possible
    Makes it easier to test Base16Options specifically.

--- tests/helpers/runPluginWithConfig.ts
+++ tests/helpers/runPluginWithConfig.ts
@@ -1,19 +1,20 @@
 import path from 'node:path'
 import postcss from 'postcss'
 import tailwind, { type Config } from 'tailwindcss'
+import { type Base16Options } from '../../src/Base16Options.ts'
 import { Base16Plugin } from '../../src/Base16Plugin.ts'
 
 type PostCSSResult = postcss.Result<postcss.Root>
 
-export async function runPluginWithConfig (config: Config, plugin = tailwind): Promise<PostCSSResult> {
+export async function runPluginWithConfig (config: Config, options?: Base16Options): Promise<PostCSSResult> {
   const { currentTestName } = expect.getState()
 
   config = {
-    ...{ plugins: [new Base16Plugin().creator], corePlugins: { preflight: false } },
+    ...{ plugins: [new Base16Plugin(options).creator], corePlugins: { preflight: false } },
     ...config
   }
 
-  return await postcss(plugin(config)).process(
+  return await postcss(tailwind(config)).process(
     [
       '@tailwind base;',
       '@tailwind components;',
