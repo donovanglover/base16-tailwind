ea4493f3da3f4528b11df0c3703869137ba9d632
Donovan Glover <donovan@dglover.co>
2024-04-17 18:49:58 -0400

    chore: Begin adding documentation for options
    Will eventually clean this up as well as document other parts of the
    code base.

--- src/Base16Options.ts
+++ src/Base16Options.ts
@@ -1,5 +1,13 @@
 export interface Base16Options {
+  /** Whether or not to style prose classes as well. */
   withTypography?: boolean
+
+  /** Whether or not to use CSS variables for fonts.
+   *
+   * TODO: Remove this since it's out of scope.
+  */
   withFontOverride?: boolean
+
+  /** Custom directory for base16 schemes in .yaml files. */
   customPath?: string
 }
