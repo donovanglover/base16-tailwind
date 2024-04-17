491c1b21f0a5088bf343c8267a5f82e5d62d9c29
Donovan Glover <donovan@dglover.co>
2024-04-17 04:26:43 -0400

    feat: Support passing options for plugin
    This change enables end users to customize the plugin without having to
    deviate from the usual way of adding Tailwind plugins.

--- src/base16Plugin.ts
+++ src/base16Plugin.ts
@@ -2,40 +2,41 @@ import path from 'node:path'
 import type { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 import type { PluginAPI, PluginCreator } from 'tailwindcss/types/config'
-import { Base16Config } from './Base16Config.ts'
+import { Base16Config, type Base16ConfigOptions } from './Base16Config.ts'
 import { getSchemesFromPath } from './getSchemesFromPath.ts'
-
-interface Base16Plugin {
-  handler: PluginCreator
-  config?: Partial<Config> | undefined
-}
 
 const schemes = getSchemesFromPath(path.join(__dirname, '../schemes/base16'))
 
-const config = new Base16Config()
+function base16Config (options?: Base16ConfigOptions): Config {
+  return new Base16Config(options)
+}
 
-export const base16Plugin: Base16Plugin = plugin(({ addUtilities }: PluginAPI): void => {
-  for (const scheme of schemes) {
-    addUtilities({
-      ['.' + scheme.name]: {
-        '--color-100': scheme.base16Colors.base07.rgb,
-        '--color-200': scheme.base16Colors.base06.rgb,
-        '--color-300': scheme.base16Colors.base05.rgb,
-        '--color-400': scheme.base16Colors.base04.rgb,
-        '--color-500': scheme.base16Colors.base03.rgb,
-        '--color-600': scheme.base16Colors.base02.rgb,
-        '--color-700': scheme.base16Colors.base01.rgb,
-        '--color-800': scheme.base16Colors.base00.rgb,
+function base16Handler (options?: Base16ConfigOptions): PluginCreator {
+  return ({ addUtilities }: PluginAPI): void => {
+    for (const scheme of schemes) {
+      addUtilities({
+        ['.' + scheme.name]: {
+          '--color-100': scheme.base16Colors.base07.rgb,
+          '--color-200': scheme.base16Colors.base06.rgb,
+          '--color-300': scheme.base16Colors.base05.rgb,
+          '--color-400': scheme.base16Colors.base04.rgb,
+          '--color-500': scheme.base16Colors.base03.rgb,
+          '--color-600': scheme.base16Colors.base02.rgb,
+          '--color-700': scheme.base16Colors.base01.rgb,
+          '--color-800': scheme.base16Colors.base00.rgb,
 
-        '--color-red': scheme.base16Colors.base08.rgb,
-        '--color-orange': scheme.base16Colors.base09.rgb,
-        '--color-yellow': scheme.base16Colors.base0A.rgb,
-        '--color-green': scheme.base16Colors.base0B.rgb,
-        '--color-cyan': scheme.base16Colors.base0C.rgb,
-        '--color-blue': scheme.base16Colors.base0D.rgb,
-        '--color-purple': scheme.base16Colors.base0E.rgb,
-        '--color-pink': scheme.base16Colors.base0F.rgb
-      }
-    })
+          '--color-red': scheme.base16Colors.base08.rgb,
+          '--color-orange': scheme.base16Colors.base09.rgb,
+          '--color-yellow': scheme.base16Colors.base0A.rgb,
+          '--color-green': scheme.base16Colors.base0B.rgb,
+          '--color-cyan': scheme.base16Colors.base0C.rgb,
+          '--color-blue': scheme.base16Colors.base0D.rgb,
+          '--color-purple': scheme.base16Colors.base0E.rgb,
+          '--color-pink': scheme.base16Colors.base0F.rgb
+        }
+      })
+    }
   }
-}, config)
+}
+
+export const base16Plugin = plugin.withOptions(base16Handler, base16Config)
