8ef350892eb9c0f5190556196926ad60f7e8cc18
Donovan Glover <donovan@dglover.co>
2024-04-18 10:21:16 -0400

    refactor(color): Limit line length; remove isValid
    BASE16_COLOR_HELP is implied since the variable is a part of the
    Base16Color class, so we've simplified it to simply "HELP"

--- src/Base16Color.ts
+++ src/Base16Color.ts
@@ -2,22 +2,21 @@ import { colord } from 'colord'
 
 export class Base16Color {
   readonly #color: string
-  static readonly #BASE16_COLOR_HELP = 'Make sure that your color is in the format "FA113D" where there are 6 hexadecimal numbers without "#" at the beginning.'
+
+  static readonly #HELP =
+    'Make sure that your color is in the format "FA113D" ' +
+    'where there are 6 hexadecimal numbers without "#" at the beginning.'
 
   constructor (maybeBase16Color: unknown) {
     if (typeof maybeBase16Color !== 'string') {
-      throw new Error(`A non-string value was given as a Base16 color. ${Base16Color.#BASE16_COLOR_HELP}`)
+      throw new Error(`A non-string value was given as a Base16 color. ${Base16Color.#HELP}`)
     }
 
-    if (!Base16Color.isValid(maybeBase16Color)) {
-      throw new Error(`Invalid Base16 color "${maybeBase16Color}" was given. ${Base16Color.#BASE16_COLOR_HELP}`)
+    if (!/^(?:#)?[0-9A-F]{6}$/i.test(maybeBase16Color)) {
+      throw new Error(`Invalid Base16 color "${maybeBase16Color}" was given. ${Base16Color.#HELP}`)
     }
 
     this.#color = maybeBase16Color.replace('#', '')
-  }
-
-  static isValid (maybeBase16Color: unknown): boolean {
-    return typeof maybeBase16Color === 'string' && /^(?:#)?[0-9A-F]{6}$/i.test(maybeBase16Color)
   }
 
   get rgb (): string {
