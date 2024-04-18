cbf399fd7ca69eaf905c08055ac816b7cec868ec
Donovan Glover <donovan@dglover.co>
2024-04-18 10:00:27 -0400

    refactor(palette): Store colors in array; remove isValid
    We don't actually care about which baseXX value a color is since the
    colors will always be in order, so we can use an array instead.
    
    This should make it easier to support other baseXX schemes in the future
    if desired, although base16 is already a lot of colors to choose from.
    
    Since we're already using a constructor that fails on errors, we can get
    rid of the isValid checks and simplify to just creating a new object
    instead. This also helps us get rid of the duplicate for loop
    iterations.

--- src/Base16Palette.ts
+++ src/Base16Palette.ts
@@ -1,60 +1,21 @@
 import { Base16Color } from './Base16Color.ts'
 
 export class Base16Palette {
-  base00: Base16Color
-  base01: Base16Color
-  base02: Base16Color
-  base03: Base16Color
-  base04: Base16Color
-  base05: Base16Color
-  base06: Base16Color
-  base07: Base16Color
-  base08: Base16Color
-  base09: Base16Color
-  base0A: Base16Color
-  base0B: Base16Color
-  base0C: Base16Color
-  base0D: Base16Color
-  base0E: Base16Color
-  base0F: Base16Color
-  static readonly #BASE16_PALETTE_KEYS = Array.from({ length: 16 }, (_, i) => `base0${i.toString(16).toUpperCase()}`)
+  colors: Base16Color[] = []
 
   constructor (maybeBase16Palette: unknown) {
-    if (!Base16Palette.isValid(maybeBase16Palette)) {
-      throw new Error('Invalid palette specified')
-    }
-
-    this.base00 = maybeBase16Palette.base00
-    this.base01 = maybeBase16Palette.base01
-    this.base02 = maybeBase16Palette.base02
-    this.base03 = maybeBase16Palette.base03
-    this.base04 = maybeBase16Palette.base04
-    this.base05 = maybeBase16Palette.base05
-    this.base06 = maybeBase16Palette.base06
-    this.base07 = maybeBase16Palette.base07
-    this.base08 = maybeBase16Palette.base08
-    this.base09 = maybeBase16Palette.base09
-    this.base0A = maybeBase16Palette.base0A
-    this.base0B = maybeBase16Palette.base0B
-    this.base0C = maybeBase16Palette.base0C
-    this.base0D = maybeBase16Palette.base0D
-    this.base0E = maybeBase16Palette.base0E
-    this.base0F = maybeBase16Palette.base0F
-  }
-
-  static isValid (maybeBase16Palette: unknown): maybeBase16Palette is Base16Palette {
     const palette = maybeBase16Palette
 
     if (palette === null || typeof palette !== 'object') {
-      return false
+      throw new Error('Palette must be an object.')
     }
 
     for (const value of Object.values(palette)) {
-      if (typeof value !== 'string' || !Base16Color.isValid(value)) {
-        return false
-      }
+      this.colors.push(new Base16Color(value))
     }
 
-    return Object.keys(palette).every((value, index) => value === this.#BASE16_PALETTE_KEYS[index])
+    if (this.colors.length < 16) {
+      throw new Error('Less than 16 colors were given.')
+    }
   }
 }
